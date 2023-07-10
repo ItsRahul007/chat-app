8e7590de9905e615ba1a2279a49911bf09dbc2a5
Rahul <rg890458@gmail.com>
2023-07-10 22:07:11 +0530

    working on setting compo

--- client/src/components/chat/homeComponents/middle-components/Setting.js
+++ client/src/components/chat/homeComponents/middle-components/Setting.js
@@ -3,9 +3,39 @@ import React from 'react'
 function Setting() {
   return (
     <div className='setting-comp'>
-      
-    </div>
-  )
-}
+      <div>Settings</div>
+      <ul className='collaps'>
+        <li>
+          <input type='radio' name='accordian' id='first' />
+          <label htmlFor='first'>General Info</label>
+          <div className='content'>
+            <div>
+              <input type='text' />
+              <div>Full Name</div>
+            </div>
+            <div>
+              <input type='text' />
+              <div>About</div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <input type='radio' name='accordian' id='second' />
+          <label htmlFor='second'>Change Password</label>
+          <div className='content'>
+            <div>
+              <input type='text' />
+              <div>Full Name</div>
+            </div>
+            <div>
+              <input type='text' />
+              <div>About</div>
+            </div>
+          </div>
+      </li>
+    </ul>
+    </div >
+  );
+};
 
 export default Setting;
