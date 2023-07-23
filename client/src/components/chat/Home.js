8046a51d7140b23fc397dbc41855c69568f14e45
Rahul <rg890458@gmail.com>
2023-07-23 12:43:10 +0530

    file name chenged

--- client/src/components/chat/Home.js
+++ client/src/components/chat/Home.js
@@ -3,10 +3,10 @@ import './home.css'
 import LeftComp from "./homeComponents/LeftComp";
 import MiddleComp from "./homeComponents/MiddleComp";
 import RightComp from "./homeComponents/RightComp";
-import Chat from './homeComponents/middle-components/Chat';
-import Profile from "./homeComponents/middle-components/Profile";
-import AvailableChat from './homeComponents/middle-components/AvailableChat';
-import Setting from './homeComponents/middle-components/Setting';
+import Chat from './homeComponents/child-components/Chat';
+import Profile from "./homeComponents/child-components/Profile";
+import AvailableChat from './homeComponents/child-components/AvailableChat';
+import Setting from './homeComponents/child-components/Setting';
 
 function Home() {
   const [compo, setCompo] = useState(<Chat key={'chat'} />);
